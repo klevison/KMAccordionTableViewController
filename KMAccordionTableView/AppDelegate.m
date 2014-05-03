a5292fbcd02624b7fc53eeff38db9d247f266c82
Jairo Junior <jairobjunior@gmail.com>
2014-05-02 23:42:26 -0300

    - Added navigation controller;

--- KMAccordionTableView/AppDelegate.m
+++ KMAccordionTableView/AppDelegate.m
@@ -13,10 +13,15 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-
+    
     KMAccordionTableViewController *accordionTableViewController = [[MyViewController alloc] init];
-    self.window.rootViewController = accordionTableViewController;
-
+    [accordionTableViewController setTitle:@"According"];
+    
+    UINavigationController *wrapNavController = [[UINavigationController alloc] initWithRootViewController:accordionTableViewController] ;
+    [wrapNavController.navigationBar setTranslucent:NO];
+    
+    self.window.rootViewController = wrapNavController;
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
@@ -27,7 +32,7 @@
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
