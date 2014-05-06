f8d3597519c2509988f2ec3226d4bdd7836d2552
Klevison Matias <klevison@gmail.com>
2014-05-06 16:01:02 -0300

    Auto Resize SubView
    setAutoresizesSubviews fix old bug

--- KMAccordionTableViewController/Classes/ViewController/KMAccordionTableViewController.m
+++ KMAccordionTableViewController/Classes/ViewController/KMAccordionTableViewController.m
@@ -56,6 +56,7 @@ static NSString *SectionHeaderViewIdentifier = @"SectionHeaderViewIdentifier";
         KMSection *section = self.sections[indexPath.section];
         [cell.contentView addSubview:section.view];
         [cell.contentView setFrame:section.view.frame];
+        [cell.contentView setAutoresizesSubviews:NO];
     }
 
     return cell;
